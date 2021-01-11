794b80e18b9d75bec2930feabfaa6ecd800fefc3
ahmeed-evan <ahmeed.3826@gmail.com>
2021-01-11 18:14:57 +0600

    fst

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button id="playButton" @click="start" :disabled="isPlaying">Play</button>
+
+  <Block v-if="isPlaying" :delay="delay"/>
+
+</template>
+
+<script>
+import Block from "./components/Block";
+
+export default {
+  name: "App",
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      console.log(this.delay)
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+#playButton {
+  color: aliceblue;
+  background: orange;
+  border-radius: 4px;
+  padding-block-start: 8px;
+  padding-block-end: 8px;
+  padding-left: 16px;
+  padding-right: 16px;
+  border: none;
+}
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    <h1>click me</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+
+  data() {
+    return {
+      showBlock: false,
+      timer: 0,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: yellowgreen;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
